dd1b72d6092227d9c2782e44b8bc0a9ffa0ea9ee
Lukasz Karpik <lkarpik@wp.pl>
2019-10-04 20:12:11 +0200

    Vertical bars

--- barChart/main.js
+++ barChart/main.js
@@ -6,11 +6,11 @@ const svg = d3.select('svg')
     .style('background-color', '#888');
 
 getData();
-
+let dataOut;
 async function getData() {
     try {
         const data = await d3.csv('data.csv');
-        const chartData = (data.map(el => {
+        const chartData = Object.entries(data.map(el => {
             return {
                 price: +el.price,
                 city: el.city
@@ -23,18 +23,18 @@ async function getData() {
             return acc;
         }, {}));
 
-        let y = d3.scaleLinear().domain([0, 2500000]).range([0, 400]);
+        let y = d3.scaleLinear().domain([0, Math.max(...chartData.map(el => el[1]))]).range([0, width / 2]);
 
 
-        console.log(Object.entries(chartData));
+        console.log(chartData);
         svg.selectAll("rect")
-            .data(Object.entries(chartData))
+            .data(chartData)
             .enter().append("rect")
             .attr("x", (d, i) => `${i * 50}px`)
             .attr("y", "0px")
             .attr("width", "50px")
             .attr("height", (d) => {
-                console.log(d);
+
                 return `${y(d[1])}px`
             })
             .attr('transform', 'translate(' + 0 + ',' + 0 + ')')
@@ -46,3 +46,41 @@ async function getData() {
         console.log(error);
     }
 }
+
+let badArray = [{
+        name: "Luck",
+        age: 33,
+        hair: "Brown"
+    },
+    {
+        name: "Buck",
+        age: 22,
+        hair: "White"
+    },
+    {
+        name: "Buck",
+        age: 5,
+        hair: "White"
+    },
+    {
+        name: "Stuck",
+        age: 22,
+        hair: "White"
+    },
+    {
+        name: "Luck",
+        age: 22,
+        hair: "White"
+    }
+];
+
+function massage() {
+    console.log(badArray);
+    return badArray.reduce((acc, el, index) => {
+
+        acc.push(index);
+        return acc;
+    }, []);
+}
+
+console.log('masage', massage());
