7d85e4dfe40ff64389076005fd4dc7ae6e6ee40a
lkarpik <lkarpik@wp.pl>
2019-10-04 14:26:16 +0200

    sandbox play

--- barChart/main.js
+++ barChart/main.js
@@ -5,12 +5,44 @@ const svg = d3.select('svg')
     .attr("height", width / 2)
     .style('background-color', '#888');
 
-let newData;
-d3.csv('data.csv').then(data => {
-    newData = data.map(el => {
-        el.price = +el.price;
-        return el
-    })
-    return data
-});
-setTimeout(() => console.log(newData), 100);
+getData();
+
+async function getData() {
+    try {
+        const data = await d3.csv('data.csv');
+        const chartData = (data.map(el => {
+            return {
+                price: +el.price,
+                city: el.city
+            }
+        }).reduce((acc, el) => {
+            if (!acc[el.city]) {
+                acc[el.city] = 0;
+            }
+            acc[el.city] += el.price;
+            return acc;
+        }, {}));
+
+        let y = d3.scaleLinear().domain([0, 2500000]).range([0, 400]);
+
+
+        console.log(Object.entries(chartData));
+        svg.selectAll("rect")
+            .data(Object.entries(chartData))
+            .enter().append("rect")
+            .attr("x", (d, i) => `${i * 50}px`)
+            .attr("y", "0px")
+            .attr("width", "50px")
+            .attr("height", (d) => {
+                console.log(d);
+                return `${y(d[1])}px`
+            })
+            .attr('transform', 'translate(' + 0 + ',' + 0 + ')')
+
+
+
+
+    } catch (error) {
+        console.log(error);
+    }
+}
